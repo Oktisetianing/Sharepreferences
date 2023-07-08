2c61328944a01e274022dea2207d7690d4d35b8a
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-07-08 12:11:02 +0700

    pertemuan 8 Juli

--- sharepreferences-main/app/src/main/java/com/example/catatpengeluaran/SharedPreferensactActivity.java
+++ sharepreferences-main/app/src/main/java/com/example/catatpengeluaran/SharedPreferensactActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import java.util.Objects;
 
 public class SharedPreferensactActivity extends AppCompatActivity {
 
@@ -62,6 +65,31 @@ public class SharedPreferensactActivity extends AppCompatActivity {
                     Toast.makeText(SharedPreferensactActivity.this,
                             "Username dan password tidak boleh kosong",
                             Toast.LENGTH_LONG).show();
+                } else {
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                           String spUsername = sharePrevManager.getUsername();
+                           String SpPassword = sharePrevManager.getPassword();
+
+                            Log.d("username", "user"+username);
+                            Log.d("password", "password"+password);
+                            if (username.equals(spUsername) && password.equals(SpPassword)){
+                                Intent i = new Intent(
+                                        SharedPreferensactActivity.this,
+                                        LoginActivity.class);
+                                sharePrevManager.saveIsLogin(true);
+                                startActivity(i);
+                            } else {
+                                pbLogin.setVisibility(View.GONE);
+                                ivLogin.setVisibility(View.VISIBLE);
+                                Toast.makeText(
+                                        SharedPreferensactActivity.this,
+                                        "Username dan Password benar",
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }, 3000);
                 }
             }
         });
